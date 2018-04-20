7c09a05c8beada21d73a5f4d53899c62a6c8ae46
Tim Luo <timluo465@gmail.com>
2018-04-20 23:08:19 +0800

    css loader

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
-import UserList from "../components/UserList";
+import UserList from "../components/user/UserList";
 
 export default [
     {path: "/", component: UserList}
